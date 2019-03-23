37a011613e3ea7354449d2c0d7323ed2f46cc52b
gaoliang <gaoliangim@gmail.com>
2019-03-23 17:39:41 +0800

    add net apis

--- server.py
+++ server.py
@@ -26,5 +26,21 @@ def card_recharge():
     return jsonify(c.recharge(amount=amount))
 
 
+@app.route('/card/net_balance', methods=['POST'])
+def card_net_balance():
+    username = request.form['username']
+    password = request.form['password']
+    c = Card(username, password)
+    return jsonify(c.get_net_balance())
+
+
+@app.route('/card/recharge_net', methods=['POST'])
+def card_recharge_net():
+    username = request.form['username']
+    password = request.form['password']
+    c = Card(username, password)
+    return jsonify(c.recharge_net())
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
