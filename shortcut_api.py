9596bd901f1e5157266f9832365a4f9c877140b0
shaoye <shaoye1124@gmail.com>
2019-03-25 01:27:18 +0800

    add auth check decorator

--- shortcut_api.py
+++ shortcut_api.py
@@ -1,7 +1,29 @@
 from flask import Flask, request, jsonify
-from njupt import Card
+from njupt import Card, Zhengfang
+from njupt.exceptions import AuthenticationException
+from functools import wraps
 
 app = Flask(__name__)
+
+
+def data_acccess(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            account = request.form['account']
+            password = request.form['password']
+            if '/card/' in request.path:
+                request.card = Card(account=account, password=password)
+            if '/zhengfang/' in request.path:
+                request.zhengfang = Zhengfang(account=account, password=password)
+            # else:
+            #     raise Exception
+            return f()
+        except AuthenticationException:
+            return jsonify({'success': False, 'message':'账号或密码有误','data':{}})
+        except Exception as e:
+            return jsonify({'success': False, 'message':e,'data':{}})
+    return wrapper
 
 
 @app.route("/")
@@ -10,37 +32,42 @@ def test():
 
 
 @app.route("/card/balance", methods=['POST'])
+@data_acccess
 def card_balance():
-    username = request.form['username']
-    password = request.form['password']
-    c = Card(account=username, password=password)
-    return jsonify(c.get_balance())
+    return jsonify(request.card.get_balance())
 
 
 @app.route("/card/recharge", methods=['POST'])
+@data_acccess
 def card_recharge():
     amount = float(request.form['amount'])
-    username = request.form['username']
-    password = request.form['password']
-    c = Card(account=username, password=password)
-    return jsonify(c.recharge(amount=amount))
+    return jsonify(request.card.recharge(amount=amount))
 
 
 @app.route('/card/net_balance', methods=['POST'])
+@data_acccess
 def card_net_balance():
-    username = request.form['username']
-    password = request.form['password']
-    c = Card(username, password)
-    return jsonify(c.get_net_balance())
+    return jsonify(request.card.get_net_balance())
 
 
 @app.route('/card/recharge_net', methods=['POST'])
+@data_acccess
 def card_recharge_net():
     amount = float(request.form['amount'])
-    username = request.form['username']
-    password = request.form['password']
-    c = Card(username, password)
-    return jsonify(c.recharge_net(amount=amount))
+    return jsonify(request.card.recharge_net(amount=amount))
+
+
+@app.route('/zhengfang/courses', methods=['POST'])
+@data_acccess
+def zhengfang_courses():
+    return jsonify(request.zhengfang.get_courses())
+
+
+@app.route('/zhengfang/courses/ical', methods=['POST'])
+@data_acccess
+def zhengfang_courses_ical():
+    # todo
+    pass
 
 
 if __name__ == "__main__":
