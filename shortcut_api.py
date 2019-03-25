90e515bfc80345fa73d0d99b9fbe946685f0897e
imguozr <imguozr@gmail.com>
2019-03-26 02:53:07 +0800

    Fix typo & Support checking morning exercise data

--- shortcut_api.py
+++ shortcut_api.py
@@ -1,35 +1,34 @@
-from flask import Flask, request, jsonify, Response
-from njupt import Card, Zhengfang
-from njupt.exceptions import AuthenticationException
-from functools import wraps
-from icalendar import Calendar, Event
-from datetime import datetime, timedelta
-import pytz
-import json
 import base64
+from datetime import datetime, timedelta
+from functools import wraps
+
+import pytz
+from flask import Flask, request, jsonify, Response
+from icalendar import Calendar, Event
+from njupt import Card, Zhengfang, RunningMan
+from njupt.exceptions import AuthenticationException
 
 term_start_date = datetime(2019, 2, 18, tzinfo=pytz.timezone("Asia/Shanghai"))
 
 app = Flask(__name__)
 
-
 ClassTime = {
-    1:  term_start_date + timedelta(hours=8),
-    2:  term_start_date + timedelta(hours=8, minutes=50),
-    3:  term_start_date + timedelta(hours=9, minutes=50),
-    4:  term_start_date + timedelta(hours=10, minutes=40),
-    5:  term_start_date + timedelta(hours=11, minutes=30),
-    6:  term_start_date + timedelta(hours=13, minutes=45),
-    7:  term_start_date + timedelta(hours=14, minutes=35),
-    8:  term_start_date + timedelta(hours=15, minutes=35),
-    9:  term_start_date + timedelta(hours=16, minutes=25),
-    10:  term_start_date + timedelta(hours=18, minutes=30),
-    11:  term_start_date + timedelta(hours=19, minutes=25),
-    12:  term_start_date + timedelta(hours=20, minutes=20),
+    1: term_start_date + timedelta(hours=8),
+    2: term_start_date + timedelta(hours=8, minutes=50),
+    3: term_start_date + timedelta(hours=9, minutes=50),
+    4: term_start_date + timedelta(hours=10, minutes=40),
+    5: term_start_date + timedelta(hours=11, minutes=30),
+    6: term_start_date + timedelta(hours=13, minutes=45),
+    7: term_start_date + timedelta(hours=14, minutes=35),
+    8: term_start_date + timedelta(hours=15, minutes=35),
+    9: term_start_date + timedelta(hours=16, minutes=25),
+    10: term_start_date + timedelta(hours=18, minutes=30),
+    11: term_start_date + timedelta(hours=19, minutes=25),
+    12: term_start_date + timedelta(hours=20, minutes=20),
 }
 
 
-def data_acccess(f):
+def data_access(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         try:
@@ -64,39 +63,39 @@ def base64_decode(string):
         content_type='text/calendar',
         headers={
             "Content-Disposition":
-            "attachment;filename=iCal.ics"
+                "attachment;filename=iCal.ics"
         }
     )
 
 
 @app.route("/card/balance", methods=['POST'])
-@data_acccess
+@data_access
 def card_balance():
     return jsonify(request.card.get_balance())
 
 
 @app.route("/card/recharge", methods=['POST'])
-@data_acccess
+@data_access
 def card_recharge():
     amount = float(request.form['amount'])
     return jsonify(request.card.recharge(amount=amount))
 
 
 @app.route('/card/net_balance', methods=['POST'])
-@data_acccess
+@data_access
 def card_net_balance():
     return jsonify(request.card.get_net_balance())
 
 
 @app.route('/card/recharge_net', methods=['POST'])
-@data_acccess
+@data_access
 def card_recharge_net():
     amount = float(request.form['amount'])
     return jsonify(request.card.recharge_net(amount=amount))
 
 
 @app.route('/card/recharge_elec', methods=['POST'])
-@data_acccess
+@data_access
 def card_recharge_elec():
     amount = float(request.form['amount'])
     school_area = request.form['school_area']
@@ -130,13 +129,13 @@ def card_recharge_elec():
 
 
 @app.route('/zhengfang/courses', methods=['POST'])
-@data_acccess
+@data_access
 def zhengfang_courses():
     return jsonify(request.zhengfang.get_courses())
 
 
 @app.route('/zhengfang/courses/ical', methods=['POST'])
-@data_acccess
+@data_access
 def zhengfang_courses_ical():
     courses = request.zhengfang.get_courses()
     cal = Calendar()
@@ -144,12 +143,12 @@ def zhengfang_courses_ical():
     cal.add('version', '2.0')
     for course in courses:
         class_start_time = ClassTime.get(course['class_start'], 1) \
-            + timedelta(days=course['day'] - 1) \
-            + timedelta(weeks=course['week_start'] - 1)
+                           + timedelta(days=course['day'] - 1) \
+                           + timedelta(weeks=course['week_start'] - 1)
         class_end_time = ClassTime.get(course['class_end'], 2) \
-            + timedelta(days=course['day'] - 1) \
-            + timedelta(weeks=course['week_start'] - 1) \
-            + timedelta(minutes=45)
+                         + timedelta(days=course['day'] - 1) \
+                         + timedelta(weeks=course['week_start'] - 1) \
+                         + timedelta(minutes=45)
         event = Event()
         event.add('summary', course['name'])
         event.add('location', course['room'] + ' ' + course['teacher'])
@@ -161,7 +160,49 @@ def zhengfang_courses_ical():
             'count': (course['week_end'] + 1) // 2
         })
         cal.add_component(event)
-    return(base64.urlsafe_b64encode(cal.to_ical()))
+    return (base64.urlsafe_b64encode(cal.to_ical()))
+
+
+@app.route('/runningman', methods=['POST'])
+def check_morning_exercise():
+    student_id = request.form['student_id']
+    name = request.form['name']
+    running_man = RunningMan(student_id, name)
+    exercise_dict = running_man.check()
+    origin_number = exercise_dict['origin_number']
+    extra_number = exercise_dict['extra_number']
+    date_list = exercise_dict['date_list']
+
+    # FIXME! NJUPT-API中return了str类型
+    total_number = int(origin_number) + int(extra_number)
+
+    # msg = ''
+    if not date_list:
+        msg = '这学期还没有跑过操'
+    if len(date_list) == 1:
+        if is_today(date_list[0]):
+            msg = '今天只打了一次卡'
+        else:
+            msg = '今天没打卡'
+    elif len(date_list) >= 2:
+        if is_same_day(date_list[0], date_list[1]) and is_today(date_list[0]):
+            msg = '今天打卡了'
+        elif is_today(date_list[0]) and not is_same_day(date_list[0], date_list[1]):
+            msg = '今天只打了一次卡'
+        else:
+            msg = '今天没打卡'
+
+    return jsonify({'total_number': total_number, 'message': msg})
+
+
+# FIXME! NJUPT-API中date_list时间正确但是时区为UTC
+def is_today(a_datetime):
+    current_date = datetime.utcnow() + timedelta(hours=8)
+    return a_datetime.date() == current_date.date()
+
+
+def is_same_day(datetime1, datetime2):
+    return datetime1.date() == datetime2.date()
 
 
 if __name__ == "__main__":
