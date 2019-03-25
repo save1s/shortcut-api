cacf19f28a0ee3b80539f0e0244386820299b3f7
shaoye <shaoye1124@gmail.com>
2019-03-25 23:11:45 +0800

    add ics download support - 02

--- shortcut_api.py
+++ shortcut_api.py
@@ -3,27 +3,29 @@ from njupt import Card, Zhengfang
 from njupt.exceptions import AuthenticationException
 from functools import wraps
 from icalendar import Calendar, Event
-import pendulum
+from datetime import datetime, timedelta
+import pytz
 import json
+import base64
 
-term_start_date = pendulum.datetime(2019, 2, 18, tz='Asia/Shanghai')
+term_start_date = datetime(2019, 2, 18, tzinfo=pytz.timezone("Asia/Shanghai"))
 
 app = Flask(__name__)
 
 
 ClassTime = {
-    1 :  term_start_date.add(hours = 8),
-    2 :  term_start_date.add(hours = 8, minutes = 50),
-    3 :  term_start_date.add(hours = 9, minutes = 50),
-    4 :  term_start_date.add(hours = 10, minutes = 40),
-    5 :  term_start_date.add(hours = 11, minutes = 30),
-    6 :  term_start_date.add(hours = 13, minutes = 45),
-    7 :  term_start_date.add(hours = 14, minutes = 35),
-    8 :  term_start_date.add(hours = 15, minutes = 35),
-    9 :  term_start_date.add(hours = 16, minutes = 25),
-    10 :  term_start_date.add(hours = 18, minutes = 30),
-    11 :  term_start_date.add(hours = 19, minutes = 25),
-    12 :  term_start_date.add(hours = 20, minutes = 20),
+    1:  term_start_date + timedelta(hours=8),
+    2:  term_start_date + timedelta(hours=8, minutes=50),
+    3:  term_start_date + timedelta(hours=9, minutes=50),
+    4:  term_start_date + timedelta(hours=10, minutes=40),
+    5:  term_start_date + timedelta(hours=11, minutes=30),
+    6:  term_start_date + timedelta(hours=13, minutes=45),
+    7:  term_start_date + timedelta(hours=14, minutes=35),
+    8:  term_start_date + timedelta(hours=15, minutes=35),
+    9:  term_start_date + timedelta(hours=16, minutes=25),
+    10:  term_start_date + timedelta(hours=18, minutes=30),
+    11:  term_start_date + timedelta(hours=19, minutes=25),
+    12:  term_start_date + timedelta(hours=20, minutes=20),
 }
 
 
@@ -36,7 +38,8 @@ def data_acccess(f):
             if '/card/' in request.path:
                 request.card = Card(account=account, password=password)
             if '/zhengfang/' in request.path:
-                request.zhengfang = Zhengfang(account=account, password=password)
+                request.zhengfang = Zhengfang(
+                    account=account, password=password)
             # else:
             #     raise Exception
             return f()
@@ -51,6 +54,19 @@ def data_acccess(f):
 @app.route("/")
 def test():
     return "A save1s.com project."
+
+
+@app.route("/base64/<string>")
+def base64_decode(string):
+    data = base64.urlsafe_b64decode(string).decode('utf-8')
+    return Response(
+        data,
+        content_type='text/calendar',
+        headers={
+            "Content-Disposition":
+            "attachment;filename=iCal.ics"
+        }
+    )
 
 
 @app.route("/card/balance", methods=['POST'])
@@ -125,18 +141,18 @@ def zhengfang_courses_ical():
     courses = request.zhengfang.get_courses()
     cal = Calendar()
     cal.add('prodid', '-//save1s.com//NJUPT Calendar//')
-    cal.add('version', '1.0')
+    cal.add('version', '2.0')
     for course in courses:
         class_start_time = ClassTime.get(course['class_start'], 1) \
-                                    .add(days = course['day'] - 1) \
-                                    .add(weeks = course['week_start'] - 1)
+            + timedelta(days=course['day'] - 1) \
+            + timedelta(weeks=course['week_start'] - 1)
         class_end_time = ClassTime.get(course['class_end'], 2) \
-                                    .add(days = course['day'] - 1) \
-                                    .add(weeks = course['week_start'] - 1) \
-                                    .add(minutes = 45)
+            + timedelta(days=course['day'] - 1) \
+            + timedelta(weeks=course['week_start'] - 1) \
+            + timedelta(minutes=45)
         event = Event()
         event.add('summary', course['name'])
-        event.add('location', course['room'] + ' ' + course['teacher'] )
+        event.add('location', course['room'] + ' ' + course['teacher'])
         event.add('dtstart', class_start_time)
         event.add('dtend', class_end_time)
         event.add('rrule', {
@@ -145,15 +161,8 @@ def zhengfang_courses_ical():
             'count': (course['week_end'] + 1) // 2
         })
         cal.add_component(event)
-        
-    return Response(
-        cal.to_ical(), 
-        content_type='text/calendar', 
-        headers={
-            "Content-Disposition": 
-            "attachment;filename=iCal.ics"
-        }
-    )
+    return(base64.urlsafe_b64encode(cal.to_ical()))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
